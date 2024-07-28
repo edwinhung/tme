464fe1d70eacc5a993fa0dbd5707af3181140d46
Edwin Hung <32249535+edwinhung@users.noreply.github.com>
2024-07-28 00:08:14 -0700

    use jsdom to simulate browser for testing

--- runner.js
+++ runner.js
@@ -2,6 +2,10 @@ const fs = require("fs");
 const path = require("path");
 const c = require("ansi-colors");
 
+const render = require("./render");
+
+const forbiddenDirs = ["node_modules"];
+
 class Runner {
   constructor() {
     this.testFiles = [];
@@ -11,6 +15,7 @@ class Runner {
     for (let file of this.testFiles) {
       console.log(c.gray(`------ ${file.shortName}`));
       const beforeEaches = [];
+      global.render = render;
       global.beforeEach = (fn) => {
         beforeEaches.push(fn);
       };
@@ -40,7 +45,7 @@ class Runner {
       const stats = await fs.promises.lstat(filepath);
       if (stats.isFile() && file.includes(".test.js")) {
         this.testFiles.push({ name: filepath, shortName: file });
-      } else if (stats.isDirectory()) {
+      } else if (stats.isDirectory() && !forbiddenDirs.includes(file)) {
         const childFiles = await fs.promises.readdir(filepath);
         files.push(...childFiles.map((f) => path.join(file, f)));
       }
